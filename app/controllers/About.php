390409ae0bc20a7d048ec50ef937d0824a368738
MuhamadAzmiNaziyulloh <azmi89017@gmail.com>
2024-02-27 16:56:32 +0700

    Belajar Mvc

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -4,11 +4,17 @@
 
 class About extends Controller{
     public function index($nama = "Muhamad Azmi Naziyulloh", $pekerjaan = "programmer"){
+        $data["judul"] = 'About';
         $data["nama"] = $nama;
         $data["pekerjaan"] = $pekerjaan;
+        $this->view('template/header', $data);
         $this->view('about/index', $data);
+        $this->view('template/footer');
     }
     public function page(){
+        $data["judul"] = 'About/Page';
+        $this->view('template/header', $data);
         $this->view('about/page');
+        $this->view('template/footer');
     }
 }
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,6 +3,9 @@
 
 class Home extends Controller {
     public function index(){
+       $data["judul"]= 'Home';
+       $this->view('template/header', $data);
        $this->view('home/index');
+       $this->view('template/footer');
     }
 }
